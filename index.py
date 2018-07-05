1d0a149e5b80a824eb1566aeee8f6586dfa3df5a
TinaFemea <tina.femea@safekick.com>
2018-07-05 16:06:37 +0100

    refresh timer, smart uuid handling

--- index.py
+++ index.py
@@ -1,15 +1,17 @@
 import os
 import datetime
 import json
+import uuid
+
 from collections import OrderedDict
 from flask import Flask, jsonify, request, render_template
 
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__,
-            static_url_path='', 
-            static_folder='static',
-            template_folder='static/templates')
+			static_url_path='', 
+			static_folder='static',
+			template_folder='static/templates')
 
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
@@ -21,23 +23,30 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class DictSerializable(object):
-    def _asdict(self):
-        result = OrderedDict()
-        for key in self.__mapper__.c.keys():
-            result[key] = getattr(self, key)
-        return result
+	def _asdict(self):
+		result = OrderedDict()
+		for key in self.__mapper__.c.keys():
+			result[key] = getattr(self, key)
+		return result
 
 class Location(db.Model, DictSerializable):
-    latitude = db.Column(db.String(80), unique=False, nullable=False, primary_key=True)
-    longitude = db.Column(db.String(80), unique=False, nullable=False, primary_key=True)
-    timestamp = db.Column(db.DateTime(), unique=False, nullable=False, primary_key=True, default=datetime.datetime.utcnow())
+	uuid = db.Column(db.String(32), unique=True, nullable=False, primary_key=True)
+	latitude = db.Column(db.Float(), unique=False, nullable=False, primary_key=False)
+	longitude = db.Column(db.Float(), unique=False, nullable=False, primary_key=False)
+	timestamp = db.Column(db.DateTime(), unique=False, nullable=False, primary_key=False)
 
-    def __repr__(self):
-        return self.toJSON
+	def __repr__(self):
+		return self.toJSON
 
-    def toJSON(self):
-    	return json.dumps(self.__dict__)
+	def toJSON(self):
+		return json.dumps(self.__dict__)
 
+def convert_java_millis(java_time_millis):
+	# Provided a java timestamp convert it into python date time object
+	ds = datetime.datetime.fromtimestamp(
+		int(str(java_time_millis)[:10])) if java_time_millis else None
+	ds = ds.replace(hour=ds.hour,minute=ds.minute,second=ds.second,microsecond=int(str(java_time_millis)[10:]) * 1000)
+	return ds	
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -61,8 +70,10 @@ def add_new():
 	print(content)
 	if 'timestamp' not in content:
 		content["timestamp"] = datetime.datetime.utcnow()
+	else:
+		content["timestamp"] = convert_java_millis(content["timestamp"])
 
-	location = Location(latitude=content["latitude"], longitude=content["longitude"], timestamp=content["timestamp"])
+	location = Location(latitude=content["latitude"], longitude=content["longitude"], timestamp=content["timestamp"], uuid=uuid.uuid1().hex)
 	db.session.add(location)
 	db.session.commit()
 	return '', 204
--- static/js/maps.js
+++ static/js/maps.js
@@ -1,43 +1,80 @@
 var map;
+var currLocs = {
+	dataList: [],
+
+	add: function(oneLoc) {
+		this.dataList[oneLoc.uuid] = oneLoc;
+	},
+
+	contains: function(uuid) {
+		return (uuid in this.dataList);
+	},
+
+ 	handleNewData: function(allTheLocs) {
+ 		var oldUUIDs = [];
+
+ 		for (var i = 0; i < Object.keys(this.dataList).length; i++) {
+ 			oldUUIDs[Object.keys(this.dataList)[i]] = 0;
+ 		}
+
+ 		for (var i = 0; i < allTheLocs.length; i++) {
+ 			if (this.contains(allTheLocs[i].uuid)) {
+ 				oldUUIDs[allTheLocs[i].uuid] = 1;
+ 			} else {
+ 				this.add(allTheLocs[i]);
+ 				addNew(allTheLocs[i]);
+ 			}
+ 		}
+
+ 		for(var i = 0; i < Object.keys(oldUUIDs).length; i++) {
+ 			var key = Object.keys(oldUUIDs)[i];
+ 			if (oldUUIDs[key] == 0) {
+ 				removeStale(this.dataList[key]);
+ 				delete this.dataList[key];
+ 			}
+ 		}
+ 	}	
+}
+
+
 
 function initMap() {
     update();
 }
 
+function addNew(currLoc) {
+	var position = {lat: Number(currLoc.latitude), lng: Number(currLoc.longitude)};
+
+	if (!map) {
+		map = new google.maps.Map(document.getElementById('map'), {
+					zoom: 15,
+					center: position});
+	}
+
+	currLoc.marker = new google.maps.Marker({
+	  position: position,
+	  map: map,
+	  title: currLoc.uuid
+	});
+
+	currLoc.tr = $('<tr>').append(
+            $('<td>').text(currLoc.latitude),
+            $('<td>').text(currLoc.longitude),
+            $('<td>').text(currLoc.timestamp)
+        );
+    $("#data tbody").append(currLoc.tr);
+}
+
+function removeStale(currLoc) {
+	currLoc.marker.setMap(null);
+	currLoc.tr.remove();
+}
+
 function update() {
 	$.ajax({url: "\locations", context: document.body}).done(function(data) {
-		addMarkers(data);
-		sidebar(data);
+		currLocs.handleNewData(data);
+
+		setTimeout(update(), 500);
 	});
-}
 
-function addMarkers(data) {
-	for(var i = 0; i < data.length; i++) {
-		var position = {lat: Number(data[i].latitude), lng: Number(data[i].longitude)};
-		
-		if (!map) {
-			map = new google.maps.Map(document.getElementById('map'), {
-						zoom: 15,
-						center: position});
-		}
-
-		var marker = new google.maps.Marker({
-		  position: position,
-		  map: map,
-		  title: 'Hello World! - ' + i
-		});
-	}
-}
-
-function sidebar(data) {
-	var body = $("#data tbody");
-
-	for(var i = 0; i < data.length; i++) {
-		var tr = $('<tr>').append(
-            $('<td>').text(data[i].latitude),
-            $('<td>').text(data[i].longitude),
-            $('<td>').text(data[i].timestamp)
-        );
-     	body.append(tr);
-     }
 }
